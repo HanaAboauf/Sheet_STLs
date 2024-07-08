77c6dc9521f7e5779ed98d8f5451f7ebce193245
HanaAboauf <142686198+HanaAboauf@users.noreply.github.com>
2024-07-08 23:22:00 +0300

    Add files via upload

---
+++ L - LR insertion .cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    deque<int>d;
+    d.push_back(n);
+
+    for(int i=s.size()-1;i>=0;--i){
+        if(s[i]=='L') {
+            d.push_back(i);
+
+        }
+        else if(s[i]=='R'){
+            d.push_front(i);
+        }
+        }
+
+        for(int item:d)
+            cout<<item<<" ";
+        cout<<"\n";
+
+    }
